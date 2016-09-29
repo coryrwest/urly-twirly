01f3c910a746679325df13b7dc43d4b878ef4136
Cory <cloud9@crwest.com>
2016-09-29 19:22:02 +0000

    Moved things around. Build dist package.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(name='urly-twirly',
+        version='1.0',
+        author='Cory Westropp',
+        author_email='cory@crwest.com',
+        url='crwest.com',
+        packages = find_packages(),
+        )
